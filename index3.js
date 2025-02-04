f6bf1bc9f79a43aff1ddd6e36c4b38ebd5ea6c11
julien <julienwatriquant01@gmail.com>
2025-02-04 16:59:31 +0100

    Browser Object Model

---
+++ index3.js
@@ -0,0 +1,7 @@
+// BOM 
+// Browser Object Model
+//   https://fr.javascript.info/browser-environment
+
+console.log(window.innerHeight);
+
+window.open("https://www.youtube.com/watch?v=bP4e5Kwz8EE&t=8465s" , "height=600, width=800");
